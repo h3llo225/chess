61df94f1433519205c34a222a51867ddf151adb7
h3llo225 <flagtop53@gmail.com>
2025-11-04 17:40:31 -0700

    starting on phase 5. hopefully got register working on the preloginUI phase

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,26 @@
 import chess.*;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import ui.preloginUI;
+
+import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataAccessException {
+        System.out.println(" Welcome. You are not signed in. Here are your options. \n");
+        preloginUI.displayOptions();
+        String resultOfChoice = new preloginUI().getChoicePrelogin();
+        while (Objects.equals(resultOfChoice, "invalid choice")){
+            System.out.println("invalid choice");
+            resultOfChoice = new preloginUI().getChoicePrelogin();
+        }
+        if (resultOfChoice == "register"){
+            String[] registerInputs = new preloginUI().getInput();
+            System.out.println(new preloginUI().registerUser(new UserData(registerInputs[0],registerInputs[1],registerInputs[2])));
+
+        }
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client: " + args[0]);
     }
 }
--- client/src/main/java/ui/preloginUI.java
+++ client/src/main/java/ui/preloginUI.java
@@ -6,26 +6,93 @@ import model.UserData;
 import service.Service;
 
 import java.util.Objects;
+import java.util.Scanner;
 
 public class preloginUI {
-    boolean assertLoggedOut(AuthData registerRequest) throws DataAccessException {
+    public boolean assertLoggedOut(AuthData registerRequest) throws DataAccessException {
         if(Objects.equals(new Service().logout(registerRequest), "{}")){
             return true;
         }else{
             return false;
         }
     }
-    boolean assertLoggedIn(UserData registerRequest) throws DataAccessException {
+    public boolean assertLoggedIn(UserData registerRequest) throws DataAccessException {
         if(new Service().login(registerRequest) != null){
             return true;
         }else{
             return false;
         }
     }
+
+    public boolean assertRegisterUser(UserData registerRequest) throws DataAccessException {
+        if(new Service().register(registerRequest) != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public String signIn(UserData registerRequest) throws DataAccessException {
         if (assertLoggedIn(registerRequest)){
+
             return String.format("You signed in as %s", registerRequest.username());
         }
         return "You are not authorized, please register an account";
+    }
+
+    public String registerUser(UserData registerRequest) throws DataAccessException {
+        if (assertRegisterUser(registerRequest)){
+            return String.format("You have registered as %s", registerRequest.username());
+        }
+        return "You are not permitted to register as this username";
+    }
+
+    public String helpPrelogin(){
+        return """
+                register(username, password, email)
+                login(username, password)
+                quit
+                """;
+    }
+
+    static public void displayOptions(){
+        System.out.println("""
+                register
+                login
+                help
+                quit
+                """);
+    }
+
+    public String getChoicePrelogin(){
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            return switch (line) {
+                case "register", "Register" -> {
+                    System.out.println("You have chosen to register! Please" +
+                            " input a username, password and email. (In that order).");
+                    yield "register";
+                }
+                case "login", "Login" -> {
+                    System.out.println("You have chosen to Login! Please" +
+                            "input a username and password. (In that order).");
+                    yield "login";
+                }
+                case "help", "Help" -> "help";
+                case "quit", "Quit" -> "quit";
+                default -> "invalid choice";
+            };
+        }
+
+    }
+
+    public String[] getInput(){
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            return line.split(" ");
+    }
+
     }
 }
