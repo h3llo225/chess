e67d9dd398d1c97f705225817b11264228421a63
h3llo225 <flagtop53@gmail.com>
2025-11-11 18:32:05 -0700

    phase 5 more error checks and stuff

--- client/src/main/java/ui/postLoginUI.java
+++ client/src/main/java/ui/postLoginUI.java
@@ -1,6 +1,8 @@
 package ui;
 
 import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
 import dataaccess.DataAccessException;
@@ -129,7 +131,49 @@ public class postLoginUI {
         return chessboardWhite;
     }
 
+public ChessPiece[][] getRealBoard(){
+    ChessPiece[][] chessboard= new ChessPiece[8][8];
+    for(int i = 0; i <8; i++){
+        for(int j = 0; j <8; j++){
+            chessboard[i][j]= new chess.ChessBoard().getPiece(new ChessPosition(i,j));
+        }
+    }
+    return chessboard;
+}
 
+//public String translator(ChessPiece piece){
+//        if (piece.getTeamColor() == ChessGame.TeamColor.BLACK){
+//            if (piece.getPieceType() == ChessPiece.PieceType.BISHOP){
+//                return EscapeSequences.BLACK_BISHOP;
+//            }
+//        }
+//}
+
+//    public String[][] initializeBoardWhiteForCustomGame(){
+//        String[][] chessboardWhite= new String[10][10];
+//        String[] labels = {"","  a  ", " b  ", " c  ", "d  ", " e  ","f  "," g  "," h  ",""};
+//        String[] ranks = {"","8", "7", "6", "5", "4","3","2","1",""};
+//        ChessPiece[][] realBoardHere = getRealBoard();
+//        for (int i = 0; i < 10; i++){
+//            chessboardWhite[i][0]=ranks[i];
+//            chessboardWhite[i][9]=ranks[i];
+//            for(int j = 0; j< 10; j++){
+//                chessboardWhite[0][j] = labels[j];
+//                chessboardWhite[9][j]=labels[j];
+//                if (i != 0 && i != 9 && j != 0 && j!= 9){
+//
+//                    chessboardWhite[i][j] = translator(realBoardHere[i][j]);
+//
+//                    chessboardWhite[i][j] = EscapeSequences.EMPTY;
+//                    // get piece here for custom board maybe
+//                }
+//            }
+//        }
+//
+//
+//
+//        return chessboardWhite;
+//    }
 
     public String[][] initializeBoardWhite(){
         String[][] chessboardWhite= new String[10][10];
@@ -293,16 +337,41 @@ public void findIDPlayHelperHelper(ArrayList<LinkedTreeMap> gamesInGameList ){
         Object correctGameID = correctGame.get("gameID");
         double newCorrectGameID = (double)correctGameID;
         TransitoryGameData newGameDataReal = new TransitoryGameData((int) newCorrectGameID, retted.playerColor().toUpperCase());
-
+    if(correctGame.get("whiteUsername") == null){
         if (Objects.equals(newGameDataReal.playerColor(), "WHITE")){
             var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
             out.print(makeChessBoard(initializeBoardWhite()));
         }
-        else if (Objects.equals(newGameDataReal.playerColor(), "BLACK")){
+    }
+        if(correctGame.get("blackUsername") == null){
+         if (Objects.equals(newGameDataReal.playerColor(), "BLACK")){
             var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
             out.print(makeChessBoard(initializeBoardBlack()));
         }
-        new serverFacade().playGame(newGameDataReal, authToken);
+        }
+        try{new serverFacade().playGame(newGameDataReal, authToken);} catch (DataAccessException e) {
+            boolean newValidInput = false;
+            while (!newValidInput){
+            System.out.println("Please input a different color");
+           String[] newColor = new preloginUI().getInput();
+           while (newColor.length != 1){
+               System.out.println("Please input a different color");
+               newColor = new preloginUI().getInput();
+           }
+           if(correctGame.get("blackUsername") == null && newColor[0].toUpperCase().equals("BLACK")){
+               newValidInput = true;
+               newGameDataReal = new TransitoryGameData((int) newCorrectGameID, newColor[0].toUpperCase());
+               new serverFacade().playGame(newGameDataReal, authToken);
+           }
+                if(correctGame.get("whiteUsername") == null && newColor[0].toUpperCase().equals("WHITE")){
+                    newValidInput = true;
+                    newGameDataReal = new TransitoryGameData((int) newCorrectGameID, newColor[0].toUpperCase());
+                    new serverFacade().playGame(newGameDataReal, authToken);
+                }
+
+
+        }
+        }
         return "game joined!";
     }
 
